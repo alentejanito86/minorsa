57f9a112f1934fde1e4119c58547855f08b779de
Sergio Antonio <sergio.miguel.andrade.antonio@gmail.comgit config --global user.email sergio.miguel.andrade.antonio@gmail.com>
2015-02-03 01:58:07 +0000

    completed cutting height

--- assets/js/machines.js
+++ assets/js/machines.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 
   FJS.addCriteria({field: 'cutting', ele: '#cutting_filter input:checkbox'});
   FJS.addCriteria({field: 'work', ele: '#genre_criteria input:checkbox'});
-  FJS.addCriteria({field: 'head', ele: '#head_filter input:checkbox'}});
+  FJS.addCriteria({field: 'head', ele: '#head_filter input:checkbox'});
   /*
   FJS.addCriteria({field: 'rating', ele: '#rating_filter', type: 'range'});
   FJS.addCriteria({field: 'runtime', ele: '#runtime_filter', type: 'range'});*/
@@ -83,30 +83,30 @@ function initSliders(){
   $('#all_genre').on('click', function(){
     $('#genre_criteria :checkbox').prop('checked', $(this).is(':checked'));
   });
-$('#cutting_filter :checkbox').prop('checked', true);
-$('#all_cutting').prop('disabled', true);
+$('#cutting_filter :checkbox').prop('checked', false);
+//$('#all_cutting').prop('disabled', true);
   $('#all_cutting').on('click', function(){
     $('#cutting_filter :checkbox').prop('checked', $(this).is(':checked'));
   });
     
- var checked = [],
-  $check = $('.check').change(function() {
-    if (this.value == "all" && this.checked) {
-       $('#all_cutting').prop('disabled', true);
-      checked = [];
-    }
-    else { 
-        $check.prop('disabled', false);
-        checked.push(this);
-        checked = $(checked)
-        checked.prop('checked', false).slice(-1).prop('checked', true);
-        $check.not(this).prop('checked', false);
-
-    }
-});
+// var checked = [],
+//  $check = $('.check').change(function() {
+//    if (this.value == "all" && this.checked) {
+//       $('#all_cutting').prop('disabled', true);
+//     checked = [];
+//    }
+//    else { 
+//        $('#all_cutting').prop('disabled', false);
+//        checked.push(this);
+//        checked = $(checked)
+//        $('#cutting_filter :checkbox').prop('checked', false).slice(-1).prop('checked', true);
+//        $check.not(this).prop('checked', false);
+//
+//    }
+//});
   
 $('#head_filter :checkbox').prop('checked', true);
-$('#all_head').prop('disabled', true);
+//$('#all_head').prop('disabled', true);
   $('#all_head').on('click', function(){
     $('#head_filter :checkbox').prop('checked', $(this).is(':checked'));
   });
