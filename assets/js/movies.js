fdbb6a62e0c11fd6b67513a0bcb638f226520c30
Sergio Antonio <sergio.miguel.andrade.antonio@gmail.comgit config --global user.email sergio.miguel.andrade.antonio@gmail.com>
2015-02-20 00:45:01 +0000

    adicionada galeria na page About

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -3,8 +3,8 @@ var movies = [
     "name": "Model MP 61",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/61/61.jpg",
-    "cutting": "1",
-    "head": "2",
+    "cutting": ["1","2"],
+    "head": ["2","3","5","6","11"],
     "page": "machines_61.html",
     "work": [
       "1",
@@ -16,8 +16,8 @@ var movies = [
     "name": "Model MP 61B",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/61b/61b.jpg",
-    "cutting": "1",
-    "head": "2",
+    "cutting": ["1","2"],
+    "head": ["2","3","6","7"],
     "page": "machines_61b.html",
     "work": [
       "1",
@@ -29,8 +29,8 @@ var movies = [
     "name": "Model MP 27A",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/27a/27a.jpg",
-    "cutting": "1",
-    "head": "2",
+    "cutting": ["1","2","3"],
+    "head": ["2","3","5","6","7","8","10","11","12"],
     "page": "machines_27a.html",
     "work": [
       "1",
@@ -42,8 +42,8 @@ var movies = [
     "name": "Model MP 27A TR",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/27a-tr/27a-tr.jpg",
-    "cutting": "1",
-    "head": "3",
+    "cutting": ["1","2","3","7","10"],
+    "head": ["2","3","4","5","6","7","8","10"],
     "page": "machines_27a-tr.html",
     "work": [
       "1",
@@ -55,8 +55,8 @@ var movies = [
     "name": "Model MP 64",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/64/64.jpg",
-    "cutting": "1",
-    "head": "4",
+    "cutting": ["1","2","3","4","5","6"],
+    "head": ["2","4","5","6","7","9"],
     "page": "machines_64.html",
     "work": [
       "1",
@@ -68,8 +68,8 @@ var movies = [
     "name": "Model MP 62",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/62/62.jpg",
-    "cutting": "1",
-    "head": "5",
+    "cutting": ["1","2","3","4","5","6","7","8"],
+    "head": ["2","4","5","6","7","8","9","10"],
     "page": "machines_62.html",
     "work": [
       "1",
@@ -81,7 +81,7 @@ var movies = [
     "name": "Model MP 62C",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/62c/62c.jpg",
-    "cutting": "1",
+    "cutting": "",
     "head": "6",
     "page": "machines_62c.html",
     "work": [
@@ -94,8 +94,8 @@ var movies = [
     "name": "Model MP 66",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/66/66.jpg",
-    "cutting": "1",
-    "head": "7",
+    "cutting": ["1","2","3","4","5","8","9"],
+    "head": ["2","4","5","6","7","8","10","12"],
     "page": "machines_66.html",
     "work": [
       "1",
@@ -107,8 +107,8 @@ var movies = [
     "name": "Model MP 12",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/12/12-3.jpg",
-    "cutting": "1",
-    "head": "6",
+    "cutting": ["3","8","9","10"],
+    "head": ["1","4","5","6","7","8","9","10"],
     "page": "machines_12.html",
     "work": [
       "1",
@@ -120,8 +120,8 @@ var movies = [
     "name": "Model MP 12 A",
     "outline": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
     "image": "machines/img/12a/12a-3.jpg",
-    "cutting": "2",
-    "head": "5",
+    "cutting": ["11","12","13","14","15","18"],
+    "head": ["4","5","6","7"],
     "page": "machines_12a.html",
     "work": [
       "2",
@@ -133,8 +133,8 @@ var movies = [
     "name": "Model TB 56",
     "outline": "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.",
     "image": "machines/img/tb56/tb56.jpg",
-    "cutting": "3",
-    "head": "4",
+    "cutting": ["18","19"],
+    "head": ["4","7"],
     "page": "#",
     "work": [
       "1",
@@ -146,8 +146,8 @@ var movies = [
     "name": "Model TB 71",
     "outline": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
     "image": "machines/img/tb71/tb71.jpg",
-    "cutting": "3",
-    "head": "3",
+    "cutting": ["18","10","23"],
+    "head": "7",
     "page": "machines_71.html",
     "work": [
       "2",
@@ -159,7 +159,7 @@ var movies = [
     "name": "Model TB 68",
     "outline": "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.",
     "image": "machines/img/tb68/tb68.jpg",
-    "cutting": "4",
+    "cutting": "20",
     "head": "2",
     "page": "#",
     "work": [
@@ -172,7 +172,7 @@ var movies = [
     "name": "Model MF 55A",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/mf55a/mf55a.jpg",
-    "cutting": "5",
+    "cutting": "23",
     "head": "1",
     "page": "machines_mf55a.html",
     "work": [
@@ -185,8 +185,8 @@ var movies = [
     "name": "Model TC 16",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/tc16/tc16.jpg",
-    "cutting": "6",
-    "head": "2",
+    "cutting": "1",
+    "head": "8",
     "page": "machines_tc16.html",
     "work": [
       "1",
@@ -198,8 +198,8 @@ var movies = [
     "name": "Model PP 06",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/pp06/pp06.jpg",
-    "cutting": "7",
-    "head": "3",
+    "cutting": "24",
+    "head": ["7","12"],
     "page": "machines_pp61b.html",
     "work": [
       "1",
@@ -211,7 +211,7 @@ var movies = [
     "name": "Model SD 58",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/sd58/sd58.jpg",
-    "cutting": "8",
+    "cutting": ["24"],
     "head": "4",
     "page": "machines_sd56.html",
     "work": [
@@ -224,7 +224,7 @@ var movies = [
     "name": "Model FL 41",
     "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     "image": "machines/img/fl41/fl41.jpg",
-    "cutting": "9",
+    "cutting": "",
     "head": "5",
     "page": "machines_fl41.html",
     "work": [
@@ -233,4 +233,5 @@ var movies = [
     ],
     "id": 1
   },
+
 ]
